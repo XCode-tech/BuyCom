3ff4cb3bbd586933fc2c26f777245f76bd6329e8
XCode-tech <xcode.tech101@gmail.com>
2024-11-20 16:12:01 +0530

    Update config.js

--- buycom/config.js
+++ buycom/config.js
@@ -1,4 +1,4 @@
 // src/config.js
-const API_URL = 'http://localhost:8000/api'; // Replace with your backend API URL
+const API_URL = 'https://buycom.onrender.com/api'; // Replace with your backend API URL
 
 export default API_URL;
